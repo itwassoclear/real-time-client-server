5a1fea36ed4eda5db10ba4fb8c82566ff5a21f30
itwassoclear <itwassoclear@gmail.com>
2023-10-03 15:59:55 +0200

    add eventsource

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './app.css';
-import LongPulling from './LongPulling';
+// import LongPulling from './LongPulling';
+import EventSourcing from './EventSourcing';
 
 function App() {
   return (
     <div>
-      <LongPulling />
+      {/* <LongPulling /> */}
+      <EventSourcing />
     </div>
   );
 }
